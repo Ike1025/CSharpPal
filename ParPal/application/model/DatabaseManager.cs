a8b3730dcad158aa5cf6b69e31e8b31198352594
Ike1025 <143562131+Ike1025@users.noreply.github.com>
2025-05-23 21:59:58 -0400

    course tostring

--- ParPal/application/model/DatabaseManager.cs
+++ ParPal/application/model/DatabaseManager.cs
@@ -3,15 +3,32 @@ class DatabaseManager
     Dictionary<int, Course> courses = [];
     Dictionary<int, Course> filteredCourses = [];
 
-    public void SearchCourse(Dictionary<string, string> request) {
+    public void SearchCourse(Dictionary<string, string> request)
+    {
+        Searcher searcher = new SearchAll();
         foreach (string key in request.Keys)
         {
             string searchType = key;
             string targetValue = request[key];
 
-            
+            switch (searchType)
+            {
+                case "name":
+                    searcher = new SearchByName(searcher, targetValue);
+                    break;
+            }
+        }
 
+        Dictionary<int, Course> result = searcher.Search(courses);
+        filteredCourses = result;
+        PrintFilteredCourses();
+    }
 
+    private void PrintFilteredCourses()
+    {
+        foreach (KeyValuePair<int, Course> pair in filteredCourses)
+        {
+            Console.WriteLine(pair.Key + ":" + pair.Value);
         }
     }
 }
--- ParPal/application/persistence/Course.cs
+++ ParPal/application/persistence/Course.cs
@@ -1,10 +1,24 @@
-class Course(int id, string name, float difficulty, string city, string state, int numOfHoles, int totalPar, List<int> parPerHole) {
-    public int Id{get;} = id;
-    public string Name{get;} = name;
-    public float Difficulty{get;} = difficulty;
-    public string City{get;} = city;
-    public string State{get;} = state;
-    public int NumOfHoles{get;} = numOfHoles;
-    public int TotalPar{get;} = totalPar;
-    public List<int> ParPerHole{get;} = parPerHole;
+class Course(int id, string name, float difficulty, string city, string state, int numOfHoles, int totalPar, List<int> parPerHole)
+{
+    public int Id { get; } = id;
+    public string Name { get; } = name;
+    public float Difficulty { get; } = difficulty;
+    public string City { get; } = city;
+    public string State { get; } = state;
+    public int NumOfHoles { get; } = numOfHoles;
+    public int TotalPar { get; } = totalPar;
+    public List<int> ParPerHole { get; } = parPerHole;
+
+    public override string ToString()
+    {
+        string holes = "";
+        for (int i=0; i < ParPerHole.Count; i++) {
+            if (i < ParPerHole.Count-1) {
+                holes += ParPerHole.Count + ";";
+            } else {
+                holes += ParPerHole.Count;
+            }
+        }
+        return Name + ", " + Difficulty + ", " + City + ", " + State + ", " + NumOfHoles + ", " + TotalPar + ", " + holes;
+    }
 }
